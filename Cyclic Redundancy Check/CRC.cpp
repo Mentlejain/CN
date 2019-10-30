35a9bf1da83758900024e5e1a7c996b46f97b515
Mr-Mentlegen <47449331+Mr-Mentlegen@users.noreply.github.com>
2019-10-30 23:52:11 +0530

    Add files via upload

--- Cyclic Redundancy Check/CRC.cpp
+++ Cyclic Redundancy Check/CRC.cpp
@@ -1,70 +1,87 @@
 #include<iostream>
 #include<conio.h>
 using namespace std;
-void division(int temp[],int gen[],int n,int r)
+void CRCD(int* msg,int m,int*gen,int g)
 {
- for(int i=0;i<n;i++)
- {
-     if (gen[0]==temp[i])
-     {
-         for(int j=0,k=i;j<r+1;j++,k++)
-             if(!(temp[k]^gen[j]))
-                 temp[k]=0;
-             else
-                 temp[k]=1;
-     } 
+	for(int i=0;i<m;i++)
+	{
+		if(msg[i]==gen[0])
+		{
+			for(int j=0,k=i;j<g;j++,k++)
+			{
+				if(msg[k]^gen[j])
+				{
+					msg[k]=1;
+				}
+				else
+				{
+					msg[k]=0;
+				}
+			}
+		}
 	}
-	for(int i=0;i<n+r+1;i++)
-		cout<<temp[i]<<" ";
+	for(int i=0;i<m+g-1;i++)
+		cout<<msg[i]<<" ";
 	cout<<endl;
 }
-
 int main()
 {
-	int n,r,message[50],gen[50],temp[50];
-	 cout<<"At Sender's End "<<endl;
-	 cout<<"Enter the number of message bits : ";
-	 cin>>n;
-	 cout<<"Enter the number of generator bits : ";
-	 cin>>r;
-	 cout<<"Enter the message : ";
-	 for(int i=0;i<n;i++)
-	     cin>>message[i];
-	 cout<<"Enter the generator : ";
-	 for(int i=0;i<r;i++)
-	     cin>>gen[i];
-	 r--;
-	 for(int i=0;i<r;i++)
-	     message[n+i] = 0;
-	 for(int i=0;i<n+r;i++)
-	     temp[i] = message[i];
-	division(temp,gen,n,r);
-	cout<<"CRC : ";
-	for(int i=0;i<r;i++)
+	int m,g;
+	cout<<"At Sender's end"<<endl;
+	cout<<"Enter the number of message bits: ";
+	cin>>m;
+	cout<<"Enter number of generator bits: ";
+	cin>>g;
+	int temp[m+g-1]={0};
+	int msg[m+g-1]={0};
+	int gen[g];
+	cout<<"Enter the message: ";
+	for(int i=0;i<m;i++)
 	{
-	    cout<<temp[n+i]<<" ";
-	    message[n+i] = temp[n+i];
+	
+		cin>>msg[i];
+		temp[i]=msg[i];
 	}
-	cout<<endl<<"Transmitted Message : ";
-	for(int i=0;i<n+r;i++)
-	    cout<<message[i]<<" ";
-	cout<<endl<<endl<<"At Receiver's End "<<endl;
-	cout<<"Enter the received message : ";
-	for(int i=0;i<n+r;i++)
-	    cin>>message[i];
-	for(int i=0;i<n+r;i++)
-	    temp[i] = message[i];
-	 division(temp,gen,n,r);
-	 for(int i=0;i<r;i++)
-	 {
-	     if(temp[n+i])
-	    {
-	        cout<<"Error detected in received message.";
-	        return 0;
-		}	
+	cout<<"Enter the generator: ";
+	for(int i=0;i<g;i++)
+		cin>>gen[i];
+	CRCD(temp,m,gen,g);
+	cout<<"CRC: ";
+	for(int i=0;i<g-1;i++)
+	{
+		cout<<temp[m+i]<<" ";
+		msg[m+i]=temp[m+i];
 	}
-	 cout<<"No error in received Message.\nReceived Message : ";
-	 for(int i=0;i<n;i++)
-	     cout<<message[i]<<" ";
-	 return 0;
+	cout<<endl;
+	cout<<"Transmitted message: ";
+	for(int i=0;i<m+g-1;i++)
+	{
+		cout<<msg[i]<<" ";
+	}
+	cout<<endl;
+	cout<<"At Receiver's end: "<<endl;
+	cout<<"Received message: ";
+	for(int i=0;i<m+g-1;i++)
+	{
+		cin>>msg[i];
+		temp[i]=msg[i];
+	}
+	CRCD(temp,m,gen,g);
+	for(int i=0;i<g-1;i++)
+	{
+		if(temp[m+i])
+		{
+			cout<<"Error in received message";
+			getch();
+			return 0;
+		}
+	}
+	cout<<"No error in received message"<<endl;
+	cout<<"Message: ";
+	for(int i=0;i<m;i++)
+	{
+		cout<<msg[i]<<" ";
+	} 
+	cout<<endl;
+	getch();
 }
